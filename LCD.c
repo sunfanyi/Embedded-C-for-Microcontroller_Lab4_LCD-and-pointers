1d2721b0ff3143cc98f6f2223bc23f563caa4b15
tangernie <ernietang1@gmail.com>
2021-11-06 22:04:57 +0000

    Completed ADC2String function to put ADC reading (out of 255) in a string, send string to LCD (Exercise 3.1)

--- LCD.c
+++ LCD.c
@@ -1,4 +1,5 @@
 #include <xc.h>
+#include <stdio.h>
 #include "LCD.h"
 
 /************************************
@@ -89,7 +90,7 @@ void LCD_Init(void)
     // entry mode set
     LCD_sendbyte(0b00000110,0);  // 0x06 Auto Increment cursor, shift display off
 	//remember to turn the LCD display back on at the end of the initialisation (not in the data sheet)
-    LCD_sendbyte(0b00001110,0);  // display on, cursor on, blinking off
+    LCD_sendbyte(0b00001100,0);  // display on, cursor off, blinking off
 }
 
 /************************************
@@ -131,6 +132,8 @@ void LCD_scroll(void)
  * Note result is stored in a buffer using pointers, it is not sent to the LCD
 ************************************/
 void ADC2String(char *buf, unsigned int ADC_val){
-	//code to calculate the inegeter and fractions part of a ADC value
+    //code to calculate the inegeter and fractions part of a ADC value
+    //unsigned int int_part = ADC_val; 
 	// and format as a string using sprintf (see GitHub readme)
+    sprintf(buf,"V = %03d", ADC_val); //put string in buffer array address, in 3 figures with leading 0's
 }
--- main.c
+++ main.c
@@ -8,30 +8,23 @@
 #include <xc.h>
 #include <stdio.h>
 #include "LCD.h"
+#include "ADC.h"
 
 #define _XTAL_FREQ 64000000 //note intrinsic _delay function is 62.5ns at 64,000,000Hz  
 
 void main(void) {
-    LCD_Init();  
+    LCD_Init(); //Initialise LCD
+    ADC_init(); //Initialise analogue input
+	unsigned int ADC_val = 0;
+    char buf[0]; //create buffer address using an array
+    
 	
-	LCD_setline(1); //Set Line 1
-//	LCD_sendbyte(0x48,1); //H
-//	LCD_sendbyte(0x65,1); //E
-//	LCD_sendbyte(0x6C,1); //L	
-//	LCD_sendbyte(0x6C,1); //L
-//	LCD_sendbyte(0x6F,1); //O
-//	LCD_sendbyte(0x20,1); //(space)
-//	LCD_sendbyte(0x57,1); //W
-//	LCD_sendbyte(0x6F,1); //O
-//	LCD_sendbyte(0x72,1); //R
-//	LCD_sendbyte(0x6C,1); //L 
-//	LCD_sendbyte(0x64,1); //D
-//	LCD_sendbyte(0x21,1); //!
-    LCD_sendstring("Hello World! Hello Again!!");
-    LCD_setline(2);
-    LCD_sendstring("Hello Again!");
-    while (1) {
-        LCD_scroll();
+    while(1){
+        ADC_val = ADC_getval(); //Read ADC voltage from LDR and place in variable
+        ADC2String(buf, ADC_val); //give buffer address and ACD reading to function for processing
+        LCD_setline(1); //Set Line 1
+        LCD_sendstring(buf); //Send string from buffer address
+        __delay_ms(300); //delay 300ms for display
     }
 }
 
